199ee68da237fe974c65dbfa00462947d6570a05
Danny Bui <dbui93@gmail.com>
2016-06-07 11:10:19 -0400

    Added node modules and changed some HTML and CSS lines

--- smashUpServer.js
+++ smashUpServer.js
@@ -1,19 +1,17 @@
 // New NodeJs Server by Danny Bui
 
 // Imports
-var http = require('http'),
-	fs = require('fs');
+var http = require('http');
+var express = require('express');
+var app = express();
+  fs = require('fs');
+app.use ('/js', express.static(__dirname + '/js'));
+app.use('/css', express.static(__dirname + '/css'));
 
-	fs. readFile('./index.html', function(err, html){
-		if (err){
-			throw err;
-		}
-		http.createServer(function(request, response){
-			response.writeHeader(200, {"Content-Type": "text/html"});
-			response.write(html);
-			response.end();
-		}).listen(8082);
-		console.log('Successfully listening on port 8082');
-	});
-
-// Function to handle request and send response
+app.all('/*', function(req, res, next) {
+    // Just send the index.html for other files to support HTML5Mode
+    res.sendFile('./index.html', { root: __dirname });
+});
+app.listen(8082, function(){
+  	console.log('Successfully listening on port 8082');
+})
