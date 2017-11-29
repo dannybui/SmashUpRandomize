0f3a8573905b57b87344c0d86cc3bccefc18060e
Danny Bui <dbui93@gmail.com>
2017-11-29 12:55:00 -0500

    test commit

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,35 @@
 import { Component } from '@angular/core';
 
+export class AppComponent {
+    title = 'List of Factions';
+    faction = 'Wizards';
+}
+
 @Component({
-  selector: 'my-app',
-  template: '<h1>SmashUp Randomizer!</h1>'
+    selector: 'my-description',
+    template: `
+      <h1>{{ocmp.title}}</h1>
+        <h2>{{ocmp.faction}}dsds</h2>`
 })
-export class AppComponent { }
+
+export class Description {
+    ocmp = new AppComponent();
+}
+
+@Component({
+    selector: 'my-header',
+    template: `
+    <h1>{{ocmp.title}}</h1>
+      <h2>{{ocmp.faction}}dsds</h2>
+        <h3><my-description></my-description></h3>`,
+    directives: [Description]
+})
+
+export class Header {
+    ocmp = new AppComponent();
+
+    constructor(){
+      console.log("Header");
+      console.log(this.ocmp);
+    }
+}
